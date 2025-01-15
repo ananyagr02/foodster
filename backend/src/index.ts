40fcf99adca77941808e055bc17698a262e57426
hiananya02@gmail.com <hiananya02@gmail.com>
2025-01-15 16:11:24 +0530

    Vercel.json added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,7 +20,13 @@ cloudinary.config({
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://food-ordering-platform.vercel.app/"],
+    methods:["POST", "GET"],
+    credentials: true
+  }
+));
 
 app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
 
